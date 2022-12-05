aaa21f0b9e2bee7a124284f9b5ed36000f07bd97
Giovanni Baquero <giovancito113@gmail.com>
2022-12-04 20:24:51 -0500

    🎨 The code was formatted

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    scrollNav();
+document.addEventListener("DOMContentLoaded", function () {
+  scrollNav();
 
-    navegacionFija();
+  navegacionFija();
 });
 
 function navegacionFija() {
+  const barra = document.querySelector(".header");
 
-    const barra = document.querySelector('.header');
+  // Registrar el Intersection Observer
+  const observer = new IntersectionObserver(function (entries) {
+    if (entries[0].isIntersecting) {
+      barra.classList.remove("fijo");
+    } else {
+      barra.classList.add("fijo");
+    }
+  });
 
-    // Registrar el Intersection Observer
-    const observer = new IntersectionObserver( function(entries) {
-        if(entries[0].isIntersecting) {
-            barra.classList.remove('fijo');
-        } else {
-            barra.classList.add('fijo');
-        }
-    });
-
-    // Elemento a observar
-    observer.observe(document.querySelector('.sobre-festival'));
+  // Elemento a observar
+  observer.observe(document.querySelector(".sobre-festival"));
 }
 
 function scrollNav() {
-    const enlaces = document.querySelectorAll('.navegacion-principal a');
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
 
-    enlaces.forEach( function( enlace ) {
-        enlace.addEventListener('click', function(e) {
-            e.preventDefault();
-            const seccion = document.querySelector(e.target.attributes.href.value);
+  enlaces.forEach(function (enlace) {
+    enlace.addEventListener("click", function (e) {
+      e.preventDefault();
+      const seccion = document.querySelector(e.target.attributes.href.value);
 
-            seccion.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+      seccion.scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 }
--- src/js/galeria.js
+++ src/js/galeria.js
@@ -1,55 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
-    crearGaleria();
+document.addEventListener("DOMContentLoaded", function () {
+  crearGaleria();
 });
 
 function crearGaleria() {
-    const galeria = document.querySelector('.galeria-imagenes');
+  const galeria = document.querySelector(".galeria-imagenes");
 
-    for( let i = 1; i <= 12; i++ ) {
-        const imagen = document.createElement('IMG');
-        imagen.src = `build/img/thumb/${i}.webp`;
-        imagen.dataset.imagenId = i;
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("IMG");
+    imagen.src = `build/img/thumb/${i}.webp`;
+    imagen.dataset.imagenId = i;
 
-        // Añadir la función de mostrarImagen
-        imagen.onclick = mostrarImagen;
+    // Añadir la función de mostrarImagen
+    imagen.onclick = mostrarImagen;
 
-       const lista = document.createElement('LI');
-       lista.appendChild(imagen);
+    const lista = document.createElement("LI");
+    lista.appendChild(imagen);
 
-       galeria.appendChild(lista);
-    }
+    galeria.appendChild(lista);
+  }
 }
 
 function mostrarImagen(e) {
-    const id = parseInt( e.target.dataset.imagenId );
+  const id = parseInt(e.target.dataset.imagenId);
 
-    // Generar la imagen
-    const imagen = document.createElement('IMG');
-    imagen.src = `build/img/grande/${id}.webp`;
+  // Generar la imagen
+  const imagen = document.createElement("IMG");
+  imagen.src = `build/img/grande/${id}.webp`;
 
-    const overlay = document.createElement('DIV');
-    overlay.appendChild(imagen);
-    overlay.classList.add('overlay');
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
 
-    // Cuando se da click, cerrar la imagen
-    overlay.onclick = function() {
-        overlay.remove();
-    }
+  // Cuando se da click, cerrar la imagen
+  overlay.onclick = function () {
+    overlay.remove();
+  };
 
-    // Boton para cerrar la imagen
-    const cerrarImagen = document.createElement('P');
-    cerrarImagen.textContent = 'X';
-    cerrarImagen.classList.add('btn-cerrar');
+  // Boton para cerrar la imagen
+  const cerrarImagen = document.createElement("P");
+  cerrarImagen.textContent = "X";
+  cerrarImagen.classList.add("btn-cerrar");
 
-    // Cuando se presiona, se cierra la imagen
-    cerrarImagen.onclick = function() {
-        overlay.remove();
-    }
+  // Cuando se presiona, se cierra la imagen
+  cerrarImagen.onclick = function () {
+    overlay.remove();
+  };
 
-    overlay.appendChild(cerrarImagen)
+  overlay.appendChild(cerrarImagen);
 
-    // Mostrar en el HTML
-    const body = document.querySelector('body');
-    body.appendChild(overlay);
-    body.classList.add('fijar-body');
+  // Mostrar en el HTML
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
